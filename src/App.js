e2acb6aa10d758062700ca1ea66b9353464329e5
Egor Zaitsev <egor.zaitsev84@gmail.com>
2023-09-13 12:47:16 +0300

    new changes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import Profile from "./components/Profile/Profile";
 import "./components/Header.css"
 import {Route, Routes} from "react-router-dom";
 import React from 'react';
-import {useTelegram} from "./hooks/useTelegram";
-import {useNavigate} from "react-router-dom";
 
 
 const App = () => {
-    const navigation = useNavigate();
-    const tg = useTelegram();
-
-    document.addEventListener('popstate', (e) => {
-        tg.BackButton.show();
-        tg.BackButton.onClick(() => {
-            navigation(-1);
-        })
-    })
 
     return (
         <>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import {useTelegram} from "../../hooks/useTelegram";
+import {useNavigate} from "react-router-dom";
 
 const Profile = () => {
     const tg = useTelegram();
+    const navigation = useNavigate();
+    tg.BackButton.show();
+    tg.BackButton.onClick(() => {
+        navigation(-1);
+    })
 
     return (
         <div>
