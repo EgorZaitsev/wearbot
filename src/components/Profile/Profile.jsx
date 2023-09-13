d5dd86d4e0a280db7e9c035e1f88d0fc99ff0630
Egor Zaitsev <egor.zaitsev84@gmail.com>
2023-09-13 14:13:43 +0300

    new changes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,17 @@
 import React, {useEffect} from 'react';
 import {useTelegram} from "../../hooks/useTelegram";
 import {BackButton} from "@twa.js/sdk";
+import {useNavigate} from "react-router-dom";
 
 const Profile = () => {
     const backbutton = new BackButton('6.8')
- const tg = useTelegram()
+    const tg = useTelegram()
+    const navigation = useNavigate()
     useEffect(() => {
-
         backbutton.show()
+        backbutton.on('click', () => {
+            navigation(-1);
+        })
     })
 
     return (
