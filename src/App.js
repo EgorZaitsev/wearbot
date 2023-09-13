5962e760c29d69ff22d84fd5aa15e95e7b51552c
Egor Zaitsev <egor.zaitsev84@gmail.com>
2023-09-13 11:59:51 +0300

    new changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from'./components/Header'
 import "./components/Header.css"
 import {useTelegram} from "./hooks/useTelegram";
 import {Link, Route, Routes} from "react-router-dom";
-import Profile from "./components/Profile/Profile";
+import Profile from "/src/components/Profile/Profile";
 import React from 'react';
 
 
