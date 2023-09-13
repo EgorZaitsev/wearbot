1b8192c7b400077ef1513ce21f912b3d00d67051
Egor Zaitsev <egor.zaitsev84@gmail.com>
2023-09-13 15:42:17 +0300

    new changes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom";
 
+
 const Profile = () => {
     const navigation = useNavigate()
-    window.Telegram.WebApp.BackButton.show();
-
-
     const backNav = () => {
         navigation(-1);
     }
+    window.Telegram.WebApp.BackButton.show();
+    window.Telegram.WebApp.BackButton.onClick(backNav);
 
     return (
         <div>
