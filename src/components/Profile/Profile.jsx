a9be568e7f0109f95a0ab0e9045346523a75d682
Egor Zaitsev <egor.zaitsev84@gmail.com>
2023-09-13 13:56:34 +0300

    new changes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,29 +1,9 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {useTelegram} from "../../hooks/useTelegram";
-import {useNavigate} from "react-router-dom";
-import {useBackButton} from "@twa.js/sdk-react";
 
 
 const Profile = () => {
-    const tg = useTelegram();
-    const navigation = useNavigate();
-    const backButton = useBackButton();
-
-
-    useEffect(() => {
-
-        backButton.on('click', () => {
-            navigation(-1);
-        })
-        backButton.show();
-
-        return () => {
-            backButton.off('click', () => {
-                console.log('alive');
-            })
-        }
-        }, [backButton, navigation])
-
+ const tg = useTelegram()
     return (
         <div>
             <p>{tg.initDataUnsafe.user}</p>
--- src/index.js
+++ src/index.js
@@ -2,46 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import {useSDK, SDKProvider} from "@twa.js/sdk-react";
 
 
-function Loader({children}) {
-    const {didInit, components, error} = useSDK();
 
-    // There were no calls of SDK's init function. It means, we did not
-    // even try to do it.
-    if (!didInit) {
-        return <div>SDK init function is not yet called.</div>;
-    }
-
-    // Error occurred during SDK init.
-    if (error !== null) {
-        return <div>Something went wrong.</div>;
-    }
-
-    // If components is null, it means, SDK is not ready at the
-    // moment and currently initializing. Usually, it takes like
-    // several milliseconds or something like that, but we should
-    // have this check.
-    if (components === null) {
-        return <div>Warming up SDK.</div>;
-    }
-
-    // Safely render application.
-    return <>{children}</>;
-}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <SDKProvider>
-          <Loader>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-          </Loader>
-      </SDKProvider>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
 
